92e1dd48508162e678bea21bf666df506fca589e
RichardMelvin019 <richardmelvin019@gmail.com>
2023-06-19 15:14:33 -0700

    low and high case

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char alphal = 'a';
+	char alphah = 'A';
+	while (alphal <= 'z')
+	{
+		putchar(alphal);
+		alphal ++;
+	}
+	while (alphah <= 'Z')
+	{
+		putchar(alphah);
+		alphah ++;
+	}
+	putchar('\n');
+	return (0);
+}
