f862cedb14e667c87ce988c330abf3dae13574a9
RichardMelvin019 <richardmelvin019@gmail.com>
2023-06-19 15:58:31 -0700

    alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Aways 0 (success)
+ */
+int main(void)
+{
+        char alpha = 'a';
+
+        while (alpha <= 'z')
+        {
+                if (alpha == 'e' || alpha == 'q')
+                {
+                        alpha++;
+                }
+                else
+                {
+                        putchar(alpha);
+                        alpha++;
+                }
+        }
+        putchar('\n');
+        return (0);
+}
