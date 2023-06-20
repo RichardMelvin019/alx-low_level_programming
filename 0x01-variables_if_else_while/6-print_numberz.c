25eaf938c95f60729f12143d5dbfc3e00e1abd0e
RichardMelvin019 <richardmelvin019@gmail.com>
2023-06-19 17:05:47 -0700

    nuberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int nz;
+
+	for (nz = 0; nz <= 9; nz++)
+	{
+		putchar(nz);
+	}
+	putchar('\n');
+	return (0);
+}
