1839e7ff58a190d367f632393f49f7adfee01e41
RichardMelvin019 <richardmelvin019@gmail.com>
2023-06-22 21:23:21 -0700

    more numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,11 +14,8 @@ void more_numbers(void)
 		for (x = 0; x <= 14; x++)
 		{
 			if (x >= 10)
-			{
-				_putchar('1');
-				_putchar(x % 10 + '0');
-			}
-			
+			_putchar('1');
+			_putchar(x % 10 + '0');
 		}
 		_putchar('\n');
 	}
