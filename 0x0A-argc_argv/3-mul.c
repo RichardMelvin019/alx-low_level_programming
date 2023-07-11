337d121258ff71865d32c6b5f1ba99f19a7dcb85
RichardMelvin019 <richardmelvin019@gmail.com>
2023-07-10 19:28:29 -0700

    mul

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,16 +12,16 @@ int main(int argc, char *argv[])
 	int num1 = 0;
 	int num2 = 0;
 
-	if (argc < 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else if (argc == 3)
+	if (argc == 3)
 	{
 		num1 = atoi(argv[1]);
 		num2 = atoi(argv[2]);
 		printf("%d\n", num1 * num2);
+	}
+	else
+	{
+		printf("Error");
+		return (0);
 	}
 	return (0);
 }
