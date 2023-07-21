8de3a43c5e7b6f01ed437712098204cf9660a350
RichardMelvin019 <richardmelvin019@gmail.com>
2023-07-20 21:47:11 -0700

    print number

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,7 +1,7 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
 /**
- * print_number - prints number
+ * print_numbers - prints number
  * @separator: string to be printed between numbers
  * @n: number of integers passed to the function
  * Return:
@@ -11,15 +11,18 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list mel_print;
 	unsigned int i;
 
-	va_start(mel_print, n);
-	for (i = 0; i < n; i++)
+	if (n > 0)
 	{
-		printf("%d", va_arg(mel_print, int));
-		if (i != n - 1 && separator != NULL)
+		va_start(mel_print, n);
+		for (i = 0; i < n; i++)
 		{
-			printf("%s", separator);
+			printf("%d", va_arg(mel_print, int));
+			if (i != n - 1 && separator != NULL)
+			{
+				printf("%s", separator);
+			}
 		}
+		va_end(mel_print);
 	}
-	va_end(mel_print);
 	printf("\n");
 }
