09737b9ca91289e7542820e5b8a63d9a65adb680
RichardMelvin019 <richardmelvin019@gmail.com>
2023-08-01 01:18:49 +0100

    updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,13 @@
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *x = h;
-	size_t i = 0;
+	size_t count = 0;
 
 	while (x != NULL)
 	{
 		printf("%d\n", x->n);
-		i += 1;
+		count += 1;
 		x = x->next;
 	}
-	return (i);
+	return (count);
 }
