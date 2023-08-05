28cd8b1b77f27b14459ae80763e5b9eab76ae1da
RichardMelvin019 <richardmelvin019@gmail.com>
2023-08-05 11:58:27 +0100

    clear bit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -10,5 +10,6 @@ void print_binary(unsigned long int n);
 unsigned long int _pow(unsigned int b, unsigned int p);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 
 #endif
