deb82935645fac6b6da6d1622a064b1b408a6531
RichardMelvin019 <richardmelvin019@gmail.com>
2023-08-05 12:12:06 +0100

    flip bits

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -11,5 +11,5 @@ unsigned long int _pow(unsigned int b, unsigned int p);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
-
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
 #endif
